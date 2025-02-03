0964f915b1156dc60b76f49d4901474e8f7559d3
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-03 20:35:04 +0700

    init express, cors, dotenv, and nodemon

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
+
+const express = require("express");
+const cors = require("cors");
+
+const whitelist = [
+  "http://localhost:3000",
+  "https://handson-panggabean-projects.firebaseapp.com",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      callback(null, true);
+    } else {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  },
+  credentials: true,
+};
+
+const app = express();
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
