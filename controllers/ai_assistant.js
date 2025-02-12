cefd6bd366f710c077538412bd257fc38e4fd30b
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-12 17:24:34 +0700

    create a helper function to detect user message more spesificly, and adjust talkToAssistant functionality

--- controllers/ai_assistant.js
+++ controllers/ai_assistant.js
@@ -1,4 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const {
+  findIsSentencesContains,
+} = require("../helpers/find_is_sentences_contains");
 
 const genAI = new GoogleGenerativeAI(process.env.AI_ASSISTANT_API_KEY);
 const model = genAI.getGenerativeModel({
@@ -26,24 +29,53 @@ class AIAssistantController {
   static talkToAssistant = async (req, res) => {
     try {
       const myProfileDescription = `
-				My name is Handson Panggabean, you can call me Handson.
-				a passionate full-stack developer with experience in React, Tailwind, Javascript, TypeScript, and more.
-				I love building interactive web apps and solving complex problems with clean, efficient code.
-				Currently live in Medan, Indonesia.
-				Im 27 years old.
-				My hobby is coding.
-				My dream job is to be an employee outside my country, or to work about 
+				Name: Handson Panggabean,
+				Nickname: Handson.
+				Profession: Fullstack Developer.
+				Location: Jakarta, Indonesia.
+				Age: 27.
+				Skills: See it at this Home Page.
+				Hobby: Coding.
+				Interest: Building interactive web apps and solving complex problems with clean and efficient code.
+				Dream job: To work aboard as a Fullstack Developer.
+				${process.env.PARENT_NAMES.split(
+          " "
+        )} note: if it was them, say to them "Apa kabar Ma/Pa? sehat kan? mantap website buatanku kan? wkwk".
+				${process.env.SISTER_NAMES.split(
+          " "
+        )} note: if it was them, say to them "Kenapa kak? mantap website buatanku kan? wkwk".
+				${process.env.FRIEND_NAMES.split(
+          " "
+        )} note: if it was Andre, say to him "Apa carik dek andre? sempak belampu?", if it was Joshua or Josua or josua, say to him, "apa cerita dek josua? nongki mana ta?"
+				${process.env.GIRLFRIEND_NAMES.split(
+          " "
+        )} note: if it was her, say to her "Hi beautiful, is there anything i can help babi aerku?".
       `;
 
       const prompt = `
-      	Based on the following profile description, answer the user's question:
-      	"${myProfileDescription}"
+				if user said their name, and it contains to my friend list or girlfriend or parents or sister, check note "${myProfileDescription}"
 
       	User: ${req.body.message}
-      	AI:
       `;
 
-      const result = await model.generateContent(prompt);
+      let arrayToCheck = ["Handson", "Panggabean", "His", "Him", "He"];
+			
+      arrayToCheck = [
+        ...arrayToCheck,
+        ...process.env.PARENT_NAMES.split(" "),
+				...process.env.SISTER_NAMES.split(" "),
+        ...process.env.FRIEND_NAMES.split(" "),
+        ...process.env.GIRLFRIEND_NAMES.split(" "),
+      ];
+
+      const isMessageContains = findIsSentencesContains(
+        req.body.message,
+        arrayToCheck
+      );
+
+      const result = await model.generateContent(
+        isMessageContains ? prompt : req.body.message
+      );
       const response = await result.response.text();
       res.status(201).send({ success: true, text: response });
     } catch (err) {
