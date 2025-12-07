5373ebed1fcd730b9b065dfac52750601d47e512
Cloudxier <hansenpanggabean8@gmail.com>
2025-12-07 14:45:20 +0700

    update ai api

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routers);
 
-app.get("/keep-server-alive", async (req, res) => {
+app.get("/check-server-status", async (req, res) => {
   try {
     res.status(200).send({ success: true, message: "Server's good to go" });
   } catch (err) {
-    console.log(err, "err <<<");
+    res.status(500).send({message: "Server is currently sleeping..."})
   }
 });
 
